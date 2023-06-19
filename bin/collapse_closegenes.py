48c4c4ca122b0d6bacc376d9d8a3cd9883510124
edg1983 <gedoardo83@gmail.com>
2023-06-19 09:10:31 +0200

    fix issue with empty tophits annotation

--- bin/collapse_closegenes.py
+++ bin/collapse_closegenes.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 
 import pandas as pd
+import shutil
 import sys
 
 # Read file from command line argument
 filename = sys.argv[1]
-df = pd.read_csv(filename, sep='\t', header=None)
+try:
+	df = pd.read_csv(filename, sep='\t', header=None)
 
-# Drop columns 20, 21, 22 and group by column 3 (variant)	
-df = df.drop(columns=[20, 21, 22])
-df[23] = df.groupby(3)[23].transform(lambda x: ','.join(x))
+	# Drop columns 20, 21, 22 and group by column 3 (variant)	
+	df = df.drop(columns=[20, 21, 22])
+	df[23] = df.groupby(3)[23].transform(lambda x: ','.join(x))
 
-# Convert back to DataFrame and drop duplicates
-result = df.drop_duplicates()
+	# Convert back to DataFrame and drop duplicates
+	result = df.drop_duplicates()
 
-# Save result to file
-result.to_csv('closegenes_collapsed.tsv', sep='\t', index=False, na_rep='NA')
+	# Save result to file
+	result.to_csv('closegenes_collapsed.tsv', sep='\t', index=False, na_rep='NA', header=False)
+
+except pd.errors.EmptyDataError:
+	print(f"Failed reading from {filename}. The file is empty")
+	shutil.copyfile(filename, "closegenes_collapsed.tsv")
